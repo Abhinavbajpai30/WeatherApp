ad6c252d4c37c079fbe4f3224aeea9899ab8e5ed
Abhinav Bajpai <Abhinav123bajpai@gmail.com>
2025-02-10 22:25:53 +0530

    Initial

---
+++ app.js
@@ -0,0 +1,49 @@
+let searchBtn = document.querySelector("#search-btn");
+let loc = document.querySelector("#location");
+let city = document.querySelector(".city");
+let temp = document.querySelector(".temp");
+let weatherIcon = document.querySelector(".weather-icon");
+let humidity = document.querySelector(".humidity")
+let wind = document.querySelector(".wind");
+let weatherCard = document.querySelector(".weather");
+let errorMsg = document.querySelector("#error-msg")
+
+let key = "";
+
+let updateWeather = async () => {
+    cityInput = loc.value.toLowerCase();
+    if(cityInput=="") cityInput="new york";
+
+    let URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${key}&units=metric`;
+
+    let rawData = await fetch(URL);
+    let data = await rawData.json();
+
+    console.log(data);
+
+    if(data.cod>=400) {
+        errorMsg.classList.remove("hide");
+        return;
+    }
+
+    if(data.cod==200) {
+        city.innerText = data.name;
+        temp.innerText = `${Math.round(data.main.temp)}°C`
+        weatherIcon.src = `images/${data.weather[0].main.toLowerCase()}.png`
+        humidity.innerText = `${data.main.humidity}%`;
+        wind.innerText = `${data.wind.speed} m/s`;
+
+        errorMsg.classList.add("hide");
+    }
+};
+
+window.onload = updateWeather;
+
+searchBtn.addEventListener("click", updateWeather);
+
+loc.addEventListener("keydown", (event) => {
+    if(event.key==="Enter") {
+        event.preventDefault();
+        searchBtn.click();
+    }
+})
