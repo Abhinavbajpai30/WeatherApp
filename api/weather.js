29f9d4a48eb31d0468537085c55036b285af63f6
Abhinav Bajpai <Abhinav123bajpai@gmail.com>
2025-02-27 06:12:01 +0530

    Vercel3

--- api/weather.js
+++ api/weather.js
@@ -1,7 +1,6 @@
 const fetch = require("node-fetch");
 module.exports = async (req, res) => {
     require("dotenv").config();
-    const app = express();
     const key = process.env.API_KEY;
 
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${req.query.city}&appid=${key}&units=metric`;
