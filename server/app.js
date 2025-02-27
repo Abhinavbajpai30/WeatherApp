987865c910fdfa8e79962bcd4755c0edad676076
Abhinav Bajpai <Abhinav123bajpai@gmail.com>
2025-02-27 06:04:53 +0530

    Vercel2

--- server/app.js
+++ server/app.js
@@ -1,21 +1,9 @@
-// const express = require('express');
-// require("dotenv").config();
+const express = require('express');
 
-// const app = express();
-// const port = process.env.PORT;
-// const key = process.env.API_KEY;
+const app = express();
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
-// app.get('/api', async (req, res) => {
-//     let url = `https://api.openweathermap.org/data/2.5/weather?q=${req.query.city}&appid=${key}&units=metric`;
-
-//     let rawData = await fetch(url);
-//     let data = await rawData.json();
-
-//     res.json(data);
-// })
-
-// app.listen(port, () => {
-//     console.log("Server is now active!");
-// });
+app.listen(port, () => {
+    console.log("Server is now active!");
+});
